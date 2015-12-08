3b10c22a92901c558e8005792392e9d01797feb1
Anita Growsoon <anitagrowsoon@gmail.com>
2015-12-08 13:13:28 -0500

    changes to server file, added routes and db

--- server.js
+++ server.js
@@ -1,25 +1,53 @@
 'use strict'
 
+// require modules
 const express    = require('express'),
       path       = require('path'),
-      logger     = require('morgan');
+      logger     = require('morgan'),
+      request    = require('request'),
+      mongoose   = require('mongoose'),
+      bodyParser = require('body-parser');
 
 
+
+// start express
 const app = express();
 
+// log all the things
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 // static files
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use('/', express.static('public'));
 
-let home    = require('./routes/homeController');
-app.use('/home', home);
+
+
+// database & mongoose
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/HoopHub');
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', (callback) => {
+  console.log('mongoose connected');
+})
+
+
+
+// controllers
+const home = require('./controllers/homeController')
+const user = require('./controllers/userController');
+
+// routes
+app.use('/', home)
+app.use('/users', user);
+
+
 
 // SERVER
 const server = app.listen(process.env.PORT || 3000, () => {
   let host = server.address().address;
   let port = server.address().port;
 
-  console.log('server running');
+  console.log('server running on port:',port);
 });
