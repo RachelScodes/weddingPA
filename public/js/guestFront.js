ac8f05bcbec3fb263e0b15c9fefb0a3e9e718332
Anita Growsoon <anitagrowsoon@gmail.com>
2015-12-17 12:24:43 -0500

    fixed styling and layout of rsvp form

--- public/js/guestFront.js
+++ public/js/guestFront.js
@@ -9,16 +9,28 @@ $(() =>{
 
    // define menu items
    $('#menu-view-guests').click( () => {
+      $('.guest-svtd').hide()
+      $('.add-edit-guest').show()
+      $('.guest-rsvp').hide()
+      $('div#guest-list').show()
       event.stopPropagation()
       console.log('show guest add edit view');
       showGuestList()
    })
    $('#menu-svtd').click( () => {
+      $('.add-edit-guest').hide()
+      $('div#guest-list').hide()
+      $('.guest-rsvp').hide()
+      $('.guest-svtd').show()
       event.stopPropagation()
       console.log('show save the date form');
       showSvtdForm()
    })
    $('#menu-rsvp').click( () => {
+      $('.add-edit-guest').hide()
+      $('div#guest-list').hide()
+      $('.guest-svtd').hide()
+      $('.guest-rsvp').show()
       event.stopPropagation()
       console.log('show rsvp form');
       showRsvpForm()
@@ -309,10 +321,11 @@ $(() =>{
       getAllGuests()
    }
 
+   function addBreak(element){
+      element.append('<br>')
+   }
+
    let showSvtdForm = function() {
-      function addBreak(element){
-         element.append('<br>')
-      }
 
       if ($('div.guest-svtd')) {
          $('div.guest-svtd').empty().remove()
@@ -320,7 +333,7 @@ $(() =>{
 
       let formDiv = $('<div>').attr('class','guest-svtd')
       $('<h2>')
-         .attr('class','rsvp-greet')
+         .attr('class','svtd-greet')
          .text('You\'re invited to ' + $('#who').text() + '\'s wedding!')
          .appendTo(formDiv);
       $('<p>')
@@ -339,7 +352,8 @@ $(() =>{
 
       let stateDropDown = $('<select>')
             .insertBefore(formDiv.children().eq(-2))
-            .append('<option selected="true" disabled="disabled">Select State:</option>');
+            .append('<option selected="true" disabled="disabled">Select State:</option>')
+            .next().css('width','100px');
 
       let stateVals = {
             "AL": 'Alabama',
@@ -431,6 +445,136 @@ $(() =>{
 
       formDiv.appendTo($('#angularize'))
    }
+
+   let showRsvpForm = function(){
+      debugger
+      // $('.guest-rsvp').hide()
+
+      if ($('div.guest-rsvp')) {
+         $('div.guest-rsvp').empty().remove()
+      }
+
+      let formDiv = $('<div>').attr('class','guest-rsvp')
+      $('<h2>')
+         .attr('class','rsvp-greet')
+         .text('You\'re invited to ' + $('#who').text() + '\'s wedding!')
+         .appendTo(formDiv);
+
+      $('<p>')
+         .text('Please enter your rsvp information below:')
+         .appendTo(formDiv);
+
+      let rsvpRadio = {
+         'Gladly Attending': 'true',
+         'Regretfully Declining': 'false',
+         'Resentfully Attending': 'true',
+         'Enthusiastically Decline': 'false',
+         'Enthusiastically Responding but won\'t show up': 'false',
+         'Will not respond but am attending anyway': 'true',
+         'I go where the free food is': 'true'
+      }
+
+      let attending = $('<div>').attr('class','attending-radios')
+         .append('<label>Will you be attending?</label>')
+         .appendTo(formDiv)
+
+      for (var i in rsvpRadio) {
+         addBreak(attending)
+         let radioButton = '<input type="radio" name="attending" value="'+rsvpRadio[i]+'">'+i
+         attending.append(radioButton)
+      }
+
+      $('<div>')
+      .append('<label>Verify/correct your name:</label>')
+      .append(
+         $('<input>')
+            .attr('type','text')
+            .attr('placeholder','Name as it should Appear'))
+      .appendTo(formDiv)
+
+      let entreeChoices = {
+         "Chicken": 'entree',
+         "Fish": 'entree',
+         "Meat": 'entree',
+         "Air": 'entree'
+      }
+
+      $('<div>').attr('class','entree-one')
+         .append('<label>Choose an entree:</label>')
+         .appendTo(formDiv)
+
+      for (var i in entreeChoices) {
+         let radioButton = '<input type="radio" name="entree" value="'+i+'">'+i
+         formDiv.children('.entree-one').append(radioButton)
+      }
+
+      $('<div>').attr('class','diet-one')
+         .append('<label>Dietary restrictions:</label>')
+         .append('<input type="text" placeholder="Kosher/Allergic to vegans"></input>')
+         .appendTo(formDiv)
+
+      let plusDate = $('<select>')
+            .append('<option selected="true" disabled="disabled">Plus One?</option>')
+            .appendTo(formDiv);
+
+      let dateVals = {
+            "true": 'Yes',
+         	"false": 'No'
+         }
+
+      for (var i in dateVals) {
+         let optionTag = $('<option>')
+            .attr('value',i)
+            .text(dateVals[i]);
+         optionTag.appendTo(plusDate);
+      };
+
+
+      $('<div>').attr('class','entree-two')
+         .append('<label>Your guest\'s entree:</label>')
+         .appendTo(formDiv)
+
+      for (var i in entreeChoices) {
+         let radioButton = '<input type="radio" name="entreep" value="'+i+'">'+i
+         formDiv.children('.entree-two').append(radioButton)
+      }
+
+      $('<div>').attr('class','diet-two')
+         .append('<label>Your guest\'s Dietary restrictions:</label>')
+         .append('<input type="text" placeholder="Kosher/Allergic to vegans"></input>')
+         .appendTo(formDiv)
+
+      $('<div>')
+         .append('<label>What song is totes obvi?</label>')
+         .append(
+            $('<input>').attr('type','text')
+               .attr('placeholder','Song Title - Artist(You Probably haven\'t heard of)'))
+         .appendTo(formDiv)
+
+      formDiv.append('<label>Anything else we should know?</label>')
+
+
+      $('<textarea>').appendTo(formDiv)
+         .attr('name','notes')
+         .attr('placeholder','Remember, you can always edit this form later');
+
+      addBreak(formDiv)
+      $('<button>')
+         .appendTo(formDiv)
+         .attr('id','rsvp-submit')
+         .text('Save Your Response')
+         .click( (e)=> {
+            debugger
+            console.log('clicked',e);
+            console.log(this);
+
+         })
+
+      formDiv.appendTo($('#angularize'))
+
+   }
+
+   // let inputFactory = function(elementType,object,attrArray){}
 
    let logEmOut = () =>{
       if (localStorage.token) {
